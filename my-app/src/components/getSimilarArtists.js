91b054ac1b585b2ffc8421befb1a9530dc18ab31
richard <theirsaviorhd@gmail.com>
2023-06-20 06:59:45 -0400

    vercel fetch bug

--- my-app/src/components/getSimilarArtists.js
+++ my-app/src/components/getSimilarArtists.js
@@ -1,7 +1,7 @@
 export async function getSimilarArtists(artistName) {
     try {
-    const apiUrl = process.env.REACT_APP_API_URL;
-    const response = await fetch(`${apiUrl}/similar-artists?artist=${encodeURIComponent(artistName)}`);
+      const apiUrl = window.location.origin;
+      const response = await fetch(`${apiUrl}/similar-artists?artist=${encodeURIComponent(artistName)}`);
       const result = await response.json();
       console.log("Success:", result);
     } catch (error) {
